0e73203aff0d3d8860ead02ad40dcf6c0e078eba
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-04-21 23:44:05 +0700

    add word_tokenize api

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
-import pythainlp
-app = Flask(__name__)
+import flask
+from pythainlp.tokenize import word_tokenize
+app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/api/v1/word_tokenize", methods=['GET','POST'])
+def word_tokenize_api():
+	if flask.request.method == 'POST':
+		source = flask.request.values.get('source')
+		engine = flask.request.values.get('engine')
+	else:
+		source = flask.request.args.get('source')
+		engine = flask.request.args.get('engine')
+	if engine==None:
+		engine='newmm'
+	return '|'.join(word_tokenize(source,engine=engine))
+
 if __name__ == '__main__':
     app.run()
