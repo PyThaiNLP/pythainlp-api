46c93f64e15c01803a56ee13c8a81a296d648bde
Pakin Pirch <pakin.pirch@gmail.com>
2020-02-29 22:56:09 +0700

    Implement tag and tokenize router

---
+++ routers/tokenize.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from pythainlp import tokenize
+
+router = APIRouter()
+
+@router.get('/tokenize/word_tokenize', tags=["tokenize"])
+def word_tokenize(q: str, engine: str = None):
+    if engine == None:
+        engine='newmm'
+    return '|'.join(tokenize.word_tokenize(q,engine=engine))
