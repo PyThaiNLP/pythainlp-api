5654cbf3288a24c1a215bb215583fbc0d6589731
Pakin Pirch <pakin.pirch@gmail.com>
2020-02-29 22:54:34 +0700

    Implement FastAPI

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 # -*- coding: utf-8 -*-
-import flask
-from pythainlp.tokenize import word_tokenize
-app = flask.Flask(__name__)
+from fastapi import Depends, FastAPI, Header, HTTPException
+from routers import tag, tokenize
 
-@app.route("/")
+app = FastAPI()
+
+@app.get("/")
 def hello():
-    return "Hello World!"
+    return {"Hello": "World"}
 
-@app.route("/api/word_tokenize", methods=['GET','POST'])
-@app.route("/api/v1/word_tokenize", methods=['GET','POST'])
-def word_tokenize_api():
-	if flask.request.method == 'POST':
-		source = flask.request.values.get('source')
-		engine = flask.request.values.get('engine')
-	else:
-		source = flask.request.args.get('source')
-		engine = flask.request.args.get('engine')
-	if engine==None:
-		engine='newmm'
-	return '|'.join(word_tokenize(source,engine=engine))
-
-if __name__ == '__main__':
-    app.run()
+app.include_router(tag.router)
+app.include_router(tokenize.router)
