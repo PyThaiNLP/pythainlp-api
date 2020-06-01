f14b250705389b0304ddb5feeb631e7fe60b334f
bact <arthit@gmail.com>
2020-06-01 23:44:09 +0100

    Update tokenize.py

--- routers/tokenize.py
+++ routers/tokenize.py
@@ -4,8 +4,9 @@ from pythainlp import tokenize
 
 router = APIRouter()
 
-@router.get('/tokenize/word_tokenize', tags=["tokenize"])
+
+@router.get("/tokenize/word_tokenize", tags=["tokenize"])
 def word_tokenize(q: str, engine: str = None):
-    if engine == None:
-        engine='newmm'
-    return '|'.join(tokenize.word_tokenize(q,engine=engine))
+    if not engine:
+        engine = "newmm"
+    return "|".join(tokenize.word_tokenize(q, engine=engine))
