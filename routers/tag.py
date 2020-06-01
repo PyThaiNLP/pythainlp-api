d88ceedb308b98659b8a47552f119a9b599c57c6
bact <arthit@gmail.com>
2020-06-01 23:45:17 +0100

    Update tag.py

--- routers/tag.py
+++ routers/tag.py
@@ -4,11 +4,12 @@ from pythainlp import tag
 
 router = APIRouter()
 
-@router.get('/tag/pos_tag', tags=["tag"])
+
+@router.get("/tag/pos_tag", tags=["tag"])
 def part_of_speech_tagging(q: str, engine: str = None, corpus: str = None):
     words = pythainlp.tokenize.word_tokenize(q)
-    if engine == None:
-        engine = 'perceptron'
-    if corpus == None:
-        corpus='orchid'
+    if not engine:
+        engine = "perceptron"
+    if not corpus:
+        corpus = "orchid"
     return tag.pos_tag(words, engine, corpus)
